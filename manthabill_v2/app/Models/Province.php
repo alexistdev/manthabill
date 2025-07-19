90e8ffbb8a4f17023feb1491ccbe31e1767e30af
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2025-07-19 18:20:24 +0700

    fix(master-province): fix id to uuid

--- manthabill_v2/app/Models/Province.php
+++ manthabill_v2/app/Models/Province.php
@@ -11,13 +11,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Province extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes,HasUuids;
 
+    protected  $primaryKey = 'id';
     protected $table = 'provinces';
     protected $fillable = ['country_id','name'];
 
--- manthabill_v2/database/migrations/2024_03_31_045934_create_provinces_table.php
+++ manthabill_v2/database/migrations/2024_03_31_045934_create_provinces_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('provinces', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->foreignUuid('country_id')
                 ->constrained('countries')
                 ->onUpdate('cascade')
